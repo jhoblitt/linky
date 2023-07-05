1cafe38e126734cfd24392d214d3cccadd21b6e4
Joshua Hoblitt <josh@hoblitt.com>
2023-07-05 15:40:59 -0700

    fwv

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jhoblitt/linky
+
+go 1.19
+
+require github.com/korovkin/limiter v0.0.0-20230307205149-3d4b2b34c99d
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/korovkin/limiter"
+)
+
+func linktest(path string, cycles int) {
+	// Create a temp directory
+	dir, err := os.MkdirTemp(path, "linktest")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < cycles; i++ {
+		orig_file := filepath.Join(dir, fmt.Sprintf("orig%d", i))
+		link_file := filepath.Join(dir, fmt.Sprintf("link%d", i))
+
+		// Create a file
+		f, err := os.Create(orig_file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f.Close()
+
+		// hard link file
+		err = os.Link(orig_file, link_file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// remove original file
+		err = os.Remove(orig_file)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func main() {
+	parallel := flag.Int("p", 10, "number of parallel operations")
+	cycles := flag.Int("n", 10, "number of file link cycles per operation")
+	flag.Parse()
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	limiter := limiter.NewConcurrencyLimiter(*parallel)
+	defer limiter.WaitAndClose()
+
+	for p := 0; p < *parallel; p++ {
+		limiter.Execute(func() {
+			linktest(path, *cycles)
+		})
+	}
+}
